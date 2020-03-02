7c9111a7b70f35f5050cb9cc1cc5284b449a5026
chenbitao <14718247979@163.com>
2020-03-02 16:41:50 +0800

    update

--- src/main/java/com/spboot/community/controller/AuthorizeController.java
+++ src/main/java/com/spboot/community/controller/AuthorizeController.java
@@ -3,6 +3,7 @@ package com.spboot.community.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.spboot.community.dto.GitHubProviderDTO;
+import com.spboot.community.mapper.UserMapper;
 import com.spboot.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("github")
@@ -25,9 +29,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUrl;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("login/callback")
     public String callback(@RequestParam("code") String code,
-                           @RequestParam("state") String state){
+                           @RequestParam("state") String state,
+                           HttpServletRequest request){
         GitHubProviderDTO gitHubProviderDTO = new GitHubProviderDTO();
         gitHubProviderDTO.setClient_id(clientId);
         gitHubProviderDTO.setClient_secret(clientSecret);
@@ -36,9 +44,14 @@ public class AuthorizeController {
         gitHubProviderDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(gitHubProviderDTO);
         String gitHubUser = gitHubProvider.GetGitHubUser(accessToken);
-        JSONObject userJson = JSON.parseObject(gitHubUser);
-        System.out.println(userJson);
-        return "index/index";
+        if(gitHubUser != null) {
+            JSONObject userJson = JSON.parseObject(gitHubUser);
+            request.getSession().setAttribute("user", userJson);
+            userMapper.insert(userJson.getString("id"),userJson.getString("name"), UUID.randomUUID().toString().replaceAll("\\-", ""));
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 
 }
--- src/main/java/com/spboot/community/mapper/UserMapper.java
+++ src/main/java/com/spboot/community/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.spboot.community.mapper;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into t_user(account,nickname,token) value(#{account},#{name},#{token})")
+    void insert(String account, String name, String token);
+
 }
